ea1688acf7eb8ae49d019404d60c048224a92405
klaassen-david <david.klaassen@web.de>
2026-01-21 01:14:33 +0100

    power management and framework-tool

--- hermes/configuration.nix
+++ hermes/configuration.nix
@@ -9,17 +9,22 @@
   nixpkgs.config.allowUnfree = true;
 
   powerManagement.enable = true;
-  services.auto-cpufreq.enable = true;
-  services.auto-cpufreq.settings = {
-    battery = {
-       governor = "powersave";
-       turbo = "never";
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
     };
-    charger = {
-       governor = "performance";
-       turbo = "auto";
-    };
-  };
+};
+
 
   users.extraUsers.dk = {
     isNormalUser = true;
@@ -109,6 +114,8 @@
     wpa_supplicant_gui
     playerctl
     calibre
+    framework-tool
+    framework-system-tools
   ];
 
   programs.dconf.enable = true;
