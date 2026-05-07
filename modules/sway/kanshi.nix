292d00f2d3494c6bc6391b94f3ba061204549297
klaassen-david <david.klaassen@web.de>
2026-05-07 16:19:21 +0200

    i3status-rust and kanshi

--- modules/sway/kanshi.nix
+++ modules/sway/kanshi.nix
@@ -2,30 +2,27 @@
   services.kanshi = {
     enable = true;
     systemdTarget = "sway-session.target";
-    profiles = {
+    settings = [
 
-      hermes-work = {
-        outputs = [
-          {
+      { profile.name = "hermes-work";
+        profile.outputs = [ {
             criteria = "BOE 0x0BC9 Unknown";
             position = "0,360";
             status = "enable";
-          }
-          {
+          } {
             criteria = "LG Electronics 24MB37 607NTNHC8631";  
             mode = "1920x1080";
             position = "2560,360";
             status = "enable";
-          }
-          {
+          } {
             criteria = "ASUSTek COMPUTER INC ASUS VA27A S9LMTF073523";
             mode = "2560x1440";
             position = "4480,0";
             status = "enable";
           }
         ];
-      };
+      }
 
-    };
+    ];
   };
 }
