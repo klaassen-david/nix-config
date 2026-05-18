8b3c52b795b3b6d0e7bf83fc29046a35f9d8a64f
klaassen-david <david.klaassen@web.de>
2026-05-18 13:45:43 +0200

    fix substituter config

--- common/common.nix
+++ common/common.nix
@@ -8,6 +8,21 @@
     "nix-command"
     "flakes"
   ];
+  nix.settings = {
+    extra-substituters = [
+      "https://nix-gaming.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://devenv.cachix.org"
+      "https://klaassen-david.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      "klaassen-david.cachix.org-1:JSXHnsFehuyyhJ+JZSRhJNlx1gCudEBCTMXLd4y1Tn8="
+    ];
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   programs.ccache.enable = true;
