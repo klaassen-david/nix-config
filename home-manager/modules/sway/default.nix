09c6384de5328af5ec0edc11e52807bb7338da99
David Klaaßen <klassed@rptu.de>
2026-05-25 21:37:22 +0200

    MOZ_ENABLE_WAYLAND=1

--- home-manager/modules/sway/default.nix
+++ home-manager/modules/sway/default.nix
@@ -26,6 +26,7 @@ in
     extraSessionCommands =
       let
         common = ''
+          export MOZ_ENABLE_WAYLAND=1
           export XDG_CURRENT_DESKTOP=sway
           export XDG_SESSION_TYPE=wayland
           export QT_QPA_PLATFORM=wayland # For Qt apps
